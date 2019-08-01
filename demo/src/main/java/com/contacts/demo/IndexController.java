6f6f0ce81e20221c543045b5624e91142d12db33
Eistern <d.zulin@g.nsu.ru>
2019-08-01 05:49:32 -0400

    Created demo for internship project

---
+++ demo/src/main/java/com/contacts/demo/IndexController.java
@@ -0,0 +1,21 @@
+package com.contacts.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+
+    @GetMapping
+    public ModelAndView index() {
+        Map<String, String> model = new HashMap<>();
+        model.put("name", "User");
+        return new ModelAndView("index", model);
+    }
+}
